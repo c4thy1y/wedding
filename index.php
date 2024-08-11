b6d8dd6f6514ab9a4d88b8c9e7d4584f9229ac2a
Cathy Ly <cl2493@nau.edu>
2024-08-11 16:25:51 -0700

    Included a yelp link

--- index.php
+++ index.php
@@ -54,7 +54,11 @@
 
 								<h2>Lễ cưới tại nhà thờ | Church Ceremony</h2>
 								<h3>Vietnamese Martyrs Catholic Church<br/>
-								2915 W Northern Ave, Phoenix, AZ 85051<br/></h3>
+								2915 W Northern Ave, Phoenix, AZ 85051<br/>
+								<a href="https://www.yelp.com/biz/vietnamese-martyrs-parish-phoenix">
+								   Nhấp vào đây để xem liên kết Yelp | Click Here for the Yelp Link
+								</a>
+							    </h3>
 								<hr style="width:100%;text-align:left;margin:0 0 10px" class="random-line">
 
 								<h3>12:00 - 12:45 PM Buổi Chụp Ảnh Trước Lễ | Pre-ceremony Photo Session</h3>
@@ -69,7 +73,11 @@
 
 								<h2>Tiệc chiêu đãi | Reception</h2>
 								<h3>Legends Event Center <br/>
-								4240 W Camelback Rd, Phoenix, AZ 85019<br/></h3>
+								4240 W Camelback Rd, Phoenix, AZ 85019<br/>
+								<a href = "https://www.yelp.com/biz/legends-event-center-phoenix">
+								   Nhấp vào đây để xem liên kết Yelp | Click Here for the Yelp Link
+								</a>
+						        </h3>
 								<hr style="width:100%;text-align:left;margin:0 0 10px" class="random-line">
 
 								<h3>5:00 PM </h3> <p>Tiệc cưới bắt đầu | Reception Starts</p>
