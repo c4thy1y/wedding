53d6146af2413ff24be89464646b6977395987d6
Cathy Ly <cl2493@nau.edu>
2024-08-11 16:01:00 -0700

    changing font size of RSVP BUTTON

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 						<nav>
 							<ul>
 								<li><a href="#info">Info | Thông Tin</a></li>
-								<li><a href="#rsvp">RSVP | Phản Hồi</a></li>
+								<li><a href="#rsvp" id = rsvp-button>RSVP | Phản Hồi</a></li>
 								<li><a href="#gallery">Gallery | Bảo Tàng</a></li>
 								<li><a href="#contact">Travel Details | Địa Diểm</a></li>
 								<!--<li><a href="#elements">Elements</a></li>-->
